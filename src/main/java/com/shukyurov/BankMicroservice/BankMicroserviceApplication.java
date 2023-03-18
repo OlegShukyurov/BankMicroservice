9b0e5b2a9583001479988f01340222f860517537
OlegShukyurov <shukyurov.oleg@gmail.com>
2023-03-18 13:11:22 +0300

    Initial commit

---
+++ src/main/java/com/shukyurov/BankMicroservice/BankMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.shukyurov.BankMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankMicroserviceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/shukyurov/BankMicroservice/BankMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shukyurov.BankMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
