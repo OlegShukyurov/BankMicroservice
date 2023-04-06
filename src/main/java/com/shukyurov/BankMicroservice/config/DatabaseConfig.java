7d43459d07d1d8a67d7d99360124f734cb1d0191
OlegShukyurov <shukyurov.oleg@gmail.com>
2023-04-07 01:22:24 +0300

    add cassandra integration

--- src/main/java/com/shukyurov/BankMicroservice/config/DatabaseConfig.java
+++ src/main/java/com/shukyurov/BankMicroservice/config/DatabaseConfig.java
@@ -1,19 +1,24 @@
 package com.shukyurov.BankMicroservice.config;
 
 import liquibase.integration.spring.SpringLiquibase;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
+
+    @Value("${spring.datasource.cassandra.jdbcUrl}")
+    private String CASSANDRA_JDBC_URL;
+
+    @Value("${spring.datasource.cassandra.driver-class-name}")
+    private String CASSANDRA_DRIVER_CLASS_NAME;
 
     @Primary
     @Bean
@@ -23,9 +28,17 @@ public class DatabaseConfig {
     }
 
     @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.cassandra")
     public DataSource cassandraDataSource() {
-       return DataSourceBuilder.create().build();
+        try {
+            Class.forName(CASSANDRA_DRIVER_CLASS_NAME);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        com.simba.cassandra.jdbc42.DataSource cassandraDataSource = new com.simba.cassandra.jdbc42.DataSource();
+
+        cassandraDataSource.setURL(CASSANDRA_JDBC_URL);
+
+        return cassandraDataSource;
     }
 
     @Bean
@@ -53,60 +66,11 @@ public class DatabaseConfig {
     private static SpringLiquibase springLiquibase(DataSource dataSource, LiquibaseProperties properties) {
         SpringLiquibase liquibase = new SpringLiquibase();
 
-        liquibase.setDataSource(dataSource);
         liquibase.setChangeLog(properties.getChangeLog());
-        liquibase.setDefaultSchema(properties.getDefaultSchema());
         liquibase.setDropFirst(properties.isDropFirst());
         liquibase.setShouldRun(properties.isEnabled());
-        liquibase.setClearCheckSums(properties.isClearChecksums());
+        liquibase.setDataSource(dataSource);
 
         return liquibase;
     }
-
-//    @Bean
-//    @ConfigurationProperties(prefix = "spring.datasource.cassandra")
-//    public DataSource cassandraDataSource() {
-//        return DataSourceBuilder.create().build();
-//        try {
-//            Class.forName("com.simba.cassandra.jdbc42.Driver");
-//        } catch (ClassNotFoundException e) {
-//            throw new RuntimeException(e);
-//        }
-//        com.simba.cassandra.jdbc42.DataSource cassandraDataSource = new com.simba.cassandra.jdbc42.DataSource();
-//
-//        cassandraDataSource.setURL("jdbc:cassandra://127.0.0.1:9042/my_keyspace;DefaultKeyspace=my_keyspace");
-//
-//        return cassandraDataSource;
-//    }
-
-//    @Bean
-//    public SpringLiquibase postgresLiquibase(@Qualifier("postgresDataSource") DataSource postgresDataSource,
-//                                             @Qualifier("postgresLiquibaseProperties") LiquibaseProperties liquibaseProperties) {
-//
-//        SpringLiquibase liquibase = new SpringLiquibase();
-//
-//        liquibase.setDataSource(postgresDataSource);
-//        liquibase.setDropFirst(liquibaseProperties.isDropFirst());
-//        liquibase.setShouldRun(liquibaseProperties.isEnabled());
-//        liquibase.setClearCheckSums(liquibaseProperties.isClearChecksums());
-//        liquibase.setChangeLog(liquibaseProperties.getChangeLog());
-//
-//        return liquibase;
-//    }
-//
-//    @Bean
-//    public SpringLiquibase cassandraLiquibase(@Qualifier("cassandraDataSource") DataSource cassandraDataSource,
-//                                              @Qualifier("cassandraLiquibaseProperties") LiquibaseProperties liquibaseProperties) {
-//
-//        SpringLiquibase liquibase = new SpringLiquibase();
-//
-//        liquibase.setDataSource(cassandraDataSource);
-//        liquibase.setDefaultSchema("my_keyspace");
-//        liquibase.setChangeLog(liquibaseProperties.getChangeLog());
-//        liquibase.setDropFirst(liquibaseProperties.isDropFirst());
-//        liquibase.setShouldRun(liquibaseProperties.isEnabled());
-//        liquibase.setClearCheckSums(liquibaseProperties.isClearChecksums());
-//
-//        return liquibase;
-//    }
 }
--- src/main/java/com/shukyurov/BankMicroservice/model/entity/ConversionEntity.java
+++ src/main/java/com/shukyurov/BankMicroservice/model/entity/ConversionEntity.java
@@ -2,6 +2,7 @@ package com.shukyurov.BankMicroservice.model.entity;
 
 import lombok.Data;
 import com.shukyurov.BankMicroservice.model.ExchangeType;
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyClass;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
 
@@ -9,14 +10,12 @@ import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-import static org.springframework.data.cassandra.core.cql.PrimaryKeyType.PARTITIONED;
-
 @Data
 @Table(name = "conversion")
 @PrimaryKeyClass
 public class ConversionEntity {
 
-    @PrimaryKeyColumn(name = "id", type = PARTITIONED)
+    @PrimaryKeyColumn(type = PrimaryKeyType.PARTITIONED)
     private Long id;
 
     @Column(name = "symbol")
